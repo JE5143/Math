eefb76dacc93e059452fbc6a15748b49124ee930
JE5143 <48130949+JE5143@users.noreply.github.com>
2019-12-06 13:36:11 -0500

    Update PasswordHandler.js

--- PasswordHandler.js
+++ PasswordHandler.js
@@ -1,2 +1,30 @@
-function login() {
+function trylogin() {
+  var pass = prompt('password');
+  if (pass == "") {
+    alert("Denied login");
+  } else if (pass) {
+    var connection = new WebSocket('wss://xd.herokuapp.com');
+    connection.onopen = function () {
+      connection.send("Hello server!");
+      var requestData = {"pass":pass}
+      connection.send(requestData);
+      console.log('opened websocket');
+    };
+    connection.onerror = function (error) {
+      console.log('a data sending error occured:' + error);
+    };
+    connection.onmessage = function (message) {
+      var data = JSON.parse(message.data);
+      var GoodLogin = data['boolean'];
+      if (GoodLogin = true) {
+        alert("Successful Login");
+        showHax();
+      } else {
+      }
+    };
+  } else {
+    alert("Denied login");
+  }
 }
+
+
