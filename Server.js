31f6b56328ca8c7cf3ea26e4bd255807c4544027
JE5143 <48130949+JE5143@users.noreply.github.com>
2019-12-06 13:53:41 -0500

    Update Server.js

--- Server.js
+++ Server.js
@@ -15,21 +15,7 @@ var server = http.createServer(function(req, res) {
                     res.end();
                 }
             });
-         } else {
-            fs.readFile("./idk.html", function(error, data) {
-                if (error) {  
-                        res.writeHead(404);  
-                        res.write(error);  
-                        res.end();  
-                    } else {  
-                        res.writeHead(200, {  
-                            'Content-Type': 'text/html'  
-                        });  
-                        res.write(data);  
-                        res.end();  
-                    } 
-            });
-         }
+  } 
 });
 const port = process.env.PORT || 5000;
 server.listen(port);
